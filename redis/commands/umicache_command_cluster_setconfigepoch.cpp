a71b994520d7c2272bf020226647e8e16a4c400b
raistmaj <raistmaj@gmail.com>
2015-10-20 13:56:29 +0200

    More commands
    Added cluster and comman commands

---
+++ redis/commands/umicache_command_cluster_setconfigepoch.cpp
@@ -0,0 +1,53 @@
+/**
+  Copyright 2015 Jose Gerardo Palma Duran. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+  EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  The views and conclusions contained in the software and documentation are
+  those of the authors and should not be interpreted as representing official
+  policies, either expressed or implied, of Jose Gerardo Palma Duran.
+*/
+#include "umicache_command_cluster_setconfigepoch.hpp"
+#include "../umicache_type_redis.hpp"
+#include <boost/lexical_cast.hpp>
+
+umi::redis::CommandClusterSetConfigEpoch::CommandClusterSetConfigEpoch(
+    int config_epoch) try : umi
+  ::redis::CommandRedis("CLUSTER",
+                        {"SET-CONFIG-EPOCH",
+                         boost::lexical_cast<std::string>(config_epoch)}) {}
+catch (boost::bad_lexical_cast &) {
+}
+
+umi::redis::CommandClusterSetConfigEpoch::~CommandClusterSetConfigEpoch() {}
+
+std::vector<uint8_t>
+umi::redis::CommandClusterSetConfigEpoch::Serialize() const {
+  umi::redis::RedisTypeArray append_command;
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeBulkString>(m_operation));
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeBulkString>(m_parameters[0]));
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeInteger>(m_parameters[1]));
+  return append_command.Serialize();
+}
