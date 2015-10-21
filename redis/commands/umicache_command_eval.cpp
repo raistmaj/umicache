0be60f0599808c30c581e828e1d0546fbe14488f
raistmaj <raistmaj@gmail.com>
2015-10-21 14:55:45 +0200

    ECHO EVAL EVALSHA EXEC EXISTS EXPIRE EXPIREAT FLUSHALL FLUSHDB commands
    ECHO EVAL EVALSHA EXEC EXISTS
    EXPIRE EXPIREAT FLUSHALL FLUSHDB commands

---
+++ redis/commands/umicache_command_eval.cpp
@@ -0,0 +1,60 @@
+/**
+  Copyright 2015 Jose Gerardo Palma Duran. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+  EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  The views and conclusions contained in the software and documentation are
+  those of the authors and should not be interpreted as representing official
+  policies, either expressed or implied, of Jose Gerardo Palma Duran.
+*/
+#include "umicache_command_eval.hpp"
+#include "../umicache_type_redis.hpp"
+#include <boost/lexical_cast.hpp>
+
+umi::redis::CommandEval::CommandEval(const std::string &script, int numkeys,
+                                     const std::vector<std::string> &keys,
+                                     const std::vector<std::string> &args) try
+    : umi
+  ::redis::CommandRedis("EVAL",
+                        {script, boost::lexical_cast<std::string>(numkeys)}) {
+    m_parameters.insert(m_parameters.end(), keys.begin(), keys.end());
+    m_parameters.insert(m_parameters.end(), args.begin(), args.end());
+  }
+catch (boost::bad_lexical_cast &) {
+}
+
+umi::redis::CommandEval::~CommandEval() {}
+
+std::vector<uint8_t> umi::redis::CommandEval::Serialize() const {
+  umi::redis::RedisTypeArray append_command;
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeBulkString>(m_operation));
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeBulkString>(m_parameters[0]));
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeInteger>(m_parameters[1]));
+  for (size_t i = 2; i < m_parameters.size(); ++i) {
+    append_command.redis_array.push_back(
+        std::make_unique<RedisTypeBulkString>(m_parameters[i]));
+  }
+  return append_command.Serialize();
+}
---
+++ redis/commands/umicache_command_exists.cpp
@@ -0,0 +1,53 @@
+/**
+  Copyright 2015 Jose Gerardo Palma Duran. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+  EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  The views and conclusions contained in the software and documentation are
+  those of the authors and should not be interpreted as representing official
+  policies, either expressed or implied, of Jose Gerardo Palma Duran.
+*/
+#include "umicache_command_exists.hpp"
+#include "../umicache_type_redis.hpp"
+#include "../../umicache_exception.hpp"
+
+umi::redis::CommandExists::CommandExists(const std::vector<std::string>& keys)
+    : umi::redis::CommandRedis("EXISTS", {}) {
+  if(!keys.empty()){
+    m_parameters.insert(m_parameters.end(), keys.begin(), keys.end());
+  }else{
+    throw umi::CacheCommandException("EXISTS", "keys are empty");
+  }
+}
+
+umi::redis::CommandExists::~CommandExists() {}
+
+std::vector<uint8_t> umi::redis::CommandExists::Serialize() const {
+  umi::redis::RedisTypeArray append_command;
+  append_command.redis_array.push_back(
+      std::make_unique<RedisTypeBulkString>(m_operation));
+  for(auto&& i: m_parameters){
+    append_command.redis_array.push_back(
+        std::make_unique<RedisTypeBulkString>(i));
+  }
+  return append_command.Serialize();
+}
